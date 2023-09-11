b9a078671cf516a7cc5ea34acf2d6eceff76de25
Omkar Ukirde <ukirdeomkar4@gmail.com>
2023-09-11 20:05:25 +0530

    first mvc

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET /movies/random
+        public IActionResult Random()
+        {
+
+            var movie = new Movie() { 
+                Name = "move",
+                Id = 1,
+            };
+
+            return View(movie);
+        }
+    }
+}
