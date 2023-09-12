c41dfb4396cb3adb1fd5a316c092f754412fa303
Omkar Ukirde <ukirdeomkar4@gmail.com>
2023-09-12 22:43:23 +0530

    update datatype for Data Object using Migrations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,9 +1,14 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer 
 
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
