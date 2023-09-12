6a0af29b89c5ea04e53b1289a2b1e5ddfee46f10
Omkar Ukirde <ukirdeomkar4@gmail.com>
2023-09-12 22:36:49 +0530

    added migrations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,8 +1,12 @@
 ﻿namespace Vidly.Models
 {
-    public class Customer
+    public class Customer 
+
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public int MembershipTypeId { get; set; }
     }
 }
---
+++ Vidly/Models/IdentityModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+namespace Vidly.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; } // My domain models
+        public DbSet<Movie> Movies { get; set; }// My domain models
+    }
+}
