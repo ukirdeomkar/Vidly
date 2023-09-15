fae8ffc8cc6ff60863bb49159fff41d43add3396
Omkar Ukirde <ukirdeomkar4@gmail.com>
2023-09-15 18:14:11 +0530

    Added Dto Class

---
+++ Vidly/Dtos/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        [Required(ErrorMessage = "Please select a valid Membership Type.")]
+        public int MembershipTypeId { get; set; }
+
+        [Class18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
